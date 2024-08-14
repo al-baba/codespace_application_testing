2f4b27d189878f02e57bae155e29024d879eb4fa
Al.C <57296226+al-baba@users.noreply.github.com>
2024-08-14 20:28:52 +0300

    Update test_application.py
    Try

--- tests/unit/test_application.py
+++ tests/unit/test_application.py
@@ -1,3 +1,12 @@
 import pytest
 
-# comment in codespace!
+from our_app import our_application_v6
+
+def estimate(kalamata):
+  departure = kalamata + 20
+  return departure 
+
+def test_estimate():
+  t_estimate = estimate(15)
+  expected = 35
+  assert t_estimate == expected 
